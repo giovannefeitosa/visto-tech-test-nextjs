131f5b93071ca2644cfc345ec9b9e6f106ca72b5
Giovanne Feitosa Afonso <giovanneafonso@gmail.com>
2023-10-31 02:02:16 -0300

    feature: responsive openai chat

--- src/components/openai/ChatUI.tsx
+++ src/components/openai/ChatUI.tsx
@@ -1,6 +1,6 @@
 import { Country } from "@/types/countries.types";
 import { ActionIcon, Textarea } from "@mantine/core";
-import { IconLoader, IconSend } from "@tabler/icons-react";
+import { IconLoader, IconSend, IconX } from "@tabler/icons-react";
 import { ChatMessage } from "./ChatMessage";
 import { ChatHistory } from "@/types/chat.types";
 import { useEffect, useRef } from "react";
@@ -36,28 +36,42 @@ export function ChatUI({
         }
     }, [history]);
 
+    const wrapperClassName = isDesktop
+        ? 'flex-1 flex flex-col md:mr-4'
+        : 'flex flex-col fixed bottom-0 left-0 right-0 top-0 bg-white z-10 md:rounded-md md:shadow-md md:border md:overflow-hidden';
+    
+    let historyClassName = 'flex-1 flex flex-col gap-2 border-x md:shadow-md p-4 overflow-y-visible overflow-hidden ';
+
+    if (isDesktop) historyClassName += 'max-h-[550px]';
+
     const onChangeMessageWrapper = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
         onChangeMessage(event.target.value.substring(0, 150));
     };
 
-    if (!isDesktop && !isVisible) return <></>;
+    const closeButton = !isDesktop && (
+        <div
+            onClick={onToggleVisibility}
+        >
+            <IconX size={24} />
+        </div>
+    );
 
-    const wrapperClassName = isDesktop
-        ? 'flex-1 flex flex-col md:mr-4'
-        : 'flex flex-col fixed bottom-0 left-0 right-0 top-0 bg-white z-10 md:rounded-md md:shadow-md md:border md:overflow-hidden'
+    if (!isDesktop && !isVisible) return <></>;
 
     return (
         <section
             className={wrapperClassName}
         >
             <div
-                className='p-2 bg-blue-500 text-white text-xl rounded-t-md flex-none font-bold'
+                className='flex p-2 bg-blue-500 text-white text-xl rounded-t-md flex-none font-bold'
             >
-                Chat about {country.officialName}
+                <p>Chat about {country.officialName}</p>
+                <div className='flex-1' />
+                {closeButton}
             </div>
             <div
                 ref={refHistory}
-                className='flex-1 flex flex-col gap-2 border-x md:shadow-md p-4 max-h-[550px] overflow-y-visible overflow-hidden'
+                className={historyClassName}
             >
                 {history.map((message, index) => (
                     <ChatMessage key={index} message={message} />
